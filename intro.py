c71de669355b20818cd12e98d4e2a9e5930c71be
Pradhyumn72 <pradhyumn.gurjar1234@gmail.com>
2025-04-22 19:13:59 +0530

    updated

---
+++ intro.py
@@ -0,0 +1,45 @@
+
+# # a= int(input("enter radius of circle :"))
+# # area= 3.14*a*a 
+# # print(area)
+
+# l=int (input(" enter val of l : "))
+# b=int (input(" enter val of b : "))
+# h=int (input(" enter val of h : "))
+# perimeter=4*(l+b+h)
+
+
+# datatypes 
+#numeric => int,float,complex
+#  string 
+# sequence=> list[],tuple(),set{}
+# key value=> dictionary{}
+# boolean => true or false
+
+# a=5+9j
+# print(type(a))
+
+#list[]
+# a=[1,2,"jazz",10,22,True] #mutable
+# print(a)
+# print(type(a))
+
+# #tuple
+# b=(1,2,True)  #non changable
+# print(b)
+# print(type(b))
+
+# sets mutuable but do not store multiple data
+# c={2,4,"hello"} 
+# print(c)
+# print(type(c))
+
+# dictionary  key can't be duplicate
+d={
+    "Name":"Bunny",
+    "Age": 23,
+    "City":"Bhopal"
+
+}
+print(d)
+print(type(d))
